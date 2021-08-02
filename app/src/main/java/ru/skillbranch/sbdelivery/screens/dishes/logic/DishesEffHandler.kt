a492673ac74a031c1effacf99fbd282935c060d5
Denis <denis0kor@gmail.com>
2021-08-02 20:35:37 +0300

    фикс для прохождения ExampleUnitTest

--- app/src/main/java/ru/skillbranch/sbdelivery/screens/dishes/logic/DishesEffHandler.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/screens/dishes/logic/DishesEffHandler.kt
@@ -23,7 +23,7 @@ class DishesEffHandler @Inject constructor(
                 commit(Msg.UpdateCartCount(count))
                 notifyChannel.send(
                     Eff.Notification.Action(
-                        message = "${effect.title} добавлен в корзину",
+                        message = "${effect.title} успешно добавлен в корзину",
                         label = "Отмена",
                         action = Msg.Dishes(
                             DishesFeature.Msg.RemoveFromCart(
@@ -55,7 +55,7 @@ class DishesEffHandler @Inject constructor(
 
 
             is DishesFeature.Eff.SearchDishes -> {
-                delay(3000)
+//                delay(3000)
                 commit(DishesFeature.Msg.ShowLoading.toMsg())
                 val dishes = repository.searchDishes(effect.query)
                 commit(DishesFeature.Msg.ShowDishes(dishes).toMsg())
